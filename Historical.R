4f5af4f72dadd70177fc69e5d6f80db93b161126
chu368354 <chu368354@hotmail.com>
2016-03-31 16:01:55 +0800

    Update Historical.R

--- Historical.R
+++ Historical.R
@@ -1,10 +1,13 @@
 #Auto download list of historical data from quantmod
-setwd("D:/R/stock/Auto_Stock")
+setwd("C:/R/yahoo_quote/")
 
-startDate<-("2014-01-01")
+#Clean up all file from folder
+file.remove(file.path("C:/R/yahoo_quote/csv/", list.files("C:/R/yahoo_quote/csv/"))) 
+
+startDate<-("2016-01-01")
 
 #create export path
-outputpath<-"D:/R/stock/Auto_Stock/csv/"
+outputpath<-"C:/R/yahoo_quote/csv/"
 
 library(sqldf)
 library(xlsx)
@@ -16,22 +19,25 @@ library(TTR)
 library(quantmod)
 
 # create list of all .csv files in folder
-rawlist<-read.csv("D:/R/stock/Auto_Stock/list.csv",header=TRUE,sep=",")
+rawlist<-read.csv("C:/R/yahoo_quote/list.csv",header=TRUE,sep=",")
 stocklist<-as.vector(rawlist[,1])
 
 #Export historical data, with error handle message
 for (i in stocklist){
   tryCatch({
-  print(i)
-  raw1 = getSymbols(Symbols = i, 
-                   src = "yahoo", 
-                   from = startDate, 
-                   auto.assign = FALSE)
-  colnames(raw1) = c("open","high","low","close","volume","adj.")
-  #change the time series to data frame
-  raw2<-data.frame(raw1,Date=index(raw1))
-  #add Symbol column
-  raw2$Symbol<-as.character(i)
-  write.zoo(raw2,paste(outputpath,i,".csv",sep=""),sep=",",row.names=FALSE)
+    print(i)
+    raw1 = getSymbols(Symbols = i, 
+                      src = "yahoo", 
+                      from = startDate, 
+                      auto.assign = FALSE)
+    colnames(raw1) = c("Open","High","Low","Close","Volume","Adj")
+    raw1$ADX<-ADX(HLC(raw1))
+    raw1$slowD<-stoch(HLC(raw1),nFastK = 9, nFastD = 3, nSlowD = 3, maType = "EMA")
+    raw1$J<-3*raw1$fastK-2*raw1$slowD
+    #change the time series to data frame
+    raw2<-data.frame(raw1,Date=index(raw1))
+    #add Symbol column
+    raw2$Symbol<-as.character(i)
+    write.zoo(raw2,paste(outputpath,i,".csv",sep=""),sep=",",row.names=FALSE)
   }, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
 }
